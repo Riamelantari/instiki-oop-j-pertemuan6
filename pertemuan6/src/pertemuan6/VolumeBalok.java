faa3508e7ba04ef2d79f345490507f96bceb958d
Riamelantari <riamelantari@gmail.com>
2025-04-15 21:50:17 +0800

    Keterangan commit

---
+++ pertemuan6/src/pertemuan6/VolumeBalok.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan6;
+
+/**
+ *
+ * @author Riamelantari
+ * TGL; 15 April 2025
+ */
+public class VolumeBalok {
+   private int panjang,lebar,tinggi;
+   private int hasil;
+   
+   public int Volume(){
+       this.hasil = this.panjang*this.lebar*this.tinggi;
+       return this.hasil; 
+   }
+    
+   public void setPanjang(int pj){
+       this.panjang = pj;
+   }
+   public void setLebar(int lb){
+       this.lebar = lb;
+   }
+   public void setTinggi(int tg){
+       this.panjang = tg;
+   }
+    
+ public int getPanjang(){
+     return this.panjang;
+ }  
+   
+  public int getLebar(){
+     return this.lebar;
+ }   
+   
+  public int getTinggi(){
+     return this.tinggi;
+ }  
+  
+  
+}
